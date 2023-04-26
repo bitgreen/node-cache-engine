e73c8a9eb808887bbe4380ecddbc3d15ef614034
rappix <rappix95@gmail.com>
2023-04-27 01:42:45 +0200

    disable cors

--- src/app.ts
+++ src/app.ts
@@ -37,7 +37,7 @@ const mainLoop = async () => {
   app.use(express.json());
   app.use(cookieParser());
 
-  app.use(cors({ credentials: true }));
+  // app.use(cors({ credentials: true }));
   app.get('/', function (req: Request, res: Response) {
     res.send('Hello from Bitgreen!');
   });
