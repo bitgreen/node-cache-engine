3f2fcb9a7876f713398210cbc9a6dc2c974b2969
rappix <rappix95@gmail.com>
2024-08-06 02:05:23 +0200

    improve logging on kyc

--- src/routes/ccMarketplace/kyc/kyc-approval.ts
+++ src/routes/ccMarketplace/kyc/kyc-approval.ts
@@ -90,8 +90,6 @@ router.post('/webhook/kyc-approval', async (req: Request, res: Response) => {
   try {
     const { type, data } = req.body;
 
-    logger.info(JSON.stringify(data))
-    logger.info(req.headers['x-fractal-signature'])
     logger.info(JSON.stringify(req.body))
 
     const signature =
